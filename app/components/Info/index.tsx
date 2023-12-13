413d7a213898ebed4dbd24eab08491c169961f8f
Hiccup19940325 <johnmichael0325@outlook.com>
2023-12-13 10:44:41 -0500

    fix the price chart

--- app/components/Info/index.tsx
+++ app/components/Info/index.tsx
@@ -18,6 +18,7 @@ const GET_PRICE = gql`
 
 export default function Info() {
     const [pairAddress, setPair] = useState(pair);
+    const [selectValue, setSelectValue] = useState('TEther/TDai')
     const { loading, error, data } = useQuery(GET_PRICE, {
         variables: {
             pair: pairAddress,
@@ -36,18 +37,21 @@ export default function Info() {
         const _pair = await Factory?.call('getPair', [token0Address, token1Address]);
         console.log(token0, token1, token0Address, token1Address, _pair)
         setPair(_pair);
+        setSelectValue(pairString)
         console.log(pairAddress)
     }
 
     if (loading) return (
-        <div className="flex flex-col gap-4 card w-[1000px] bg-slate-800">
-            <div className="flex gap-4 items-center mt-10 ml-10">
-                <div className="flex flex-col gap-4">
-                    <div className="skeleton h-4 w-20"></div>
-                    <div className="skeleton h-4 w-28"></div>
-                </div>
+        <div className="card w-[1000px] bg-slate-800 shadow-xl">
+            <div className="card-body">
+                <h2 className="card-title text-white">
+                    TEther Price Info
+                </h2>
+                <select className="select select-bordered select-sm w-[150px]" value={selectValue} onChange={(e) => getPair(e.target.value)}>
+                    {pairs.map(item => <option value={`${item[0]}/${item[1]}`}>{`${item[0]}/${item[1]}`}</option>)}
+                </select>
+                <div className="skeleton h-[500px] w-[900px] m-10"></div>
             </div>
-            <div className="skeleton h-[500px] w-[900px] m-10"></div>
         </div>
     );
     if (error) return <p className="text-white">{error.message}</p>;
@@ -80,7 +84,7 @@ export default function Info() {
                     <h2 className="card-title text-white">
                         TEther Price Info
                     </h2>
-                    <select className="select select-bordered select-sm w-[150px]" onChange={(e) => getPair(e.target.value)}>
+                    <select className="select select-bordered select-sm w-[150px]" value={selectValue} onChange={(e) => getPair(e.target.value)}>
                         {pairs.map(item => <option value={`${item[0]}/${item[1]}`}>{`${item[0]}/${item[1]}`}</option>)}
                     </select>
                     <PlotChart data={chartConfig.data} type={chartConfig.type} />
